a973e540e1150c1db5dd02b8ee48d55086e99695
Victoria Munteanu <munteanuvictoria1@gmail.com>
2025-02-05 10:23:15 +0100

    update routes and add form

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
 
 all_books = []
 
+app.config['SECRET_KEY'] = "230853496459jgirfngdf8"
+
+class newBookForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    author = StringField('Author', validators=[DataRequired()])
+    submit = SubmitField('Add')
+
+
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    pass
+    form = newBookForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        author = form.author.data
+
+        all_books.append({"title": title, "author": author})
+         
+        return redirect(url_for("home"))
+
+    return render_template("add.html", form=form)
+
+
+
+    
 
 
 if __name__ == "__main__":
