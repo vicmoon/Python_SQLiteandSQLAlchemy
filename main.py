d570e78be2ea96e3fc9f336ad83f026f1c8597fd
Victoria Munteanu <munteanuvictoria1@gmail.com>
2025-02-05 09:35:57 +0100

    update

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 
